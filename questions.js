069e2a7fcbd4060b3b024e52bd8216e98138b25b
EddyBautista <bautista.j.eddy@gmail.com>
2020-07-26 21:34:57 -0500

    will build out more

--- questions.js
+++ questions.js
@@ -13,3 +13,18 @@ const ask = (i = 0) => {
 
 
 ask();
+//intl a empty array 
+const answers = [];
+
+
+// waiting for a data event 
+// push the console response to the answer string and if it is les then it will take the answers length as a var and pass it to the ask method 
+process.stdin.on('data', data => {
+    answers.push(data.toString().trim());
+    if(answers.length < questions.length){
+        ask(answers.length);
+    } else{
+        process.exit();    
+    }
+    
+});
