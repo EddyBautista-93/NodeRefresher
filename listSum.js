e952bfb9b009e0fdcb510c3b86913cdf4d3a5098
EddyBautista <bautista.j.eddy@gmail.com>
2020-07-28 22:17:11 -0500

    need to remove NaN;

--- listSum.js
+++ listSum.js
@@ -3,3 +3,24 @@
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 // Bonus: Can you do this in one pass?
+
+
+const listSum = ( array, sum ) => {
+    var sumArray = [];
+    for( let i = 0; i < array.length; i++ ) {
+        console.log( array[i]  + array[i + 1]);
+        for (let j = i + 1; j < array.length;j++){
+            if(array[i] + array[j] === sum){
+                sumArray.push([array[i], array[j]])
+            }
+        }
+    }        
+}
+
+
+
+let myStringArray = [10, 15, 3, 7];
+let numberSum = 17;
+listSum(myStringArray,numberSum);
+
+
