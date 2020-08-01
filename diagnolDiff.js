430fdf233f60c5331db4f34c966cbd6a2c35b442
EddyBautista <bautista.j.eddy@gmail.com>
2020-08-01 12:09:31 -0500

    Saturday Hackerank Question 0801

---
+++ diagnolDiff.js
@@ -0,0 +1,9 @@
+function diagonalDifference(arr) {
+    // get the length of the array 
+    const dim = arr[0].length;
+
+    //create the two vars for the diagonal sums 
+    let firstDiagonalSum = 0;
+    let secondDiagonalSum = 0;
+
+}
